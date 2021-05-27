83b57ee30cbf44c81c907b396000d423dec613c6
Lingjun Meng <575628304@qq.com>
2021-05-27 15:38:39 +0800

    Update PlayerControllerTest.java

--- src/back-end/src/test/java/com/cg/backend/PlayerControllerTest.java
+++ src/back-end/src/test/java/com/cg/backend/PlayerControllerTest.java
@@ -4,6 +4,7 @@ import com.cg.backend.controller.PlayerController;
 import com.cg.backend.dao.PlayerMapper;
 import com.cg.backend.model.Player;
 import com.cg.backend.service.PlayerService;
+import java.util.List;
 import javax.annotation.Resource;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
@@ -17,8 +18,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class PlayerControllerTest {
 //  @Resource
 //  PlayerMapper playerMapper;
-//  @Resource
-//  PlayerService playerService;
+  @Resource
+  PlayerService playerService;
   @Resource
   PlayerController playerController;
 
@@ -29,5 +30,19 @@ public class PlayerControllerTest {
         "lead", "John", "");
     playerController.updatePlayer(player);
   }
+
+  @Test
+  public void addPlayer(){
+    Player player = new Player((long) 6, "James Rumbold", "jamesR@gmail.com",
+        "male", "0412345678", "Mon,Sun",
+        "lead", "John", "");
+    playerController.updatePlayer(player);
+  }
+
+  @Test
+  public void getAllPlayer(){
+    this.playerService.selectAll();
+  }
+
 
 }
