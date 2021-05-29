f9506658ebda0229dbae8c85016f72983bbba1fc
cyprinida <49174086+cyprinida@users.noreply.github.com>
2021-05-29 15:33:47 +0800

    update tests

--- src/back-end/src/test/java/com/cg/backend/PlayerControllerTest.java
+++ src/back-end/src/test/java/com/cg/backend/PlayerControllerTest.java
@@ -14,6 +14,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -53,6 +56,12 @@ public class PlayerControllerTest {
   public void testGetAllPlayer(){
     this.playerService.selectAll();
   }
+
+  @Test
+  public void testDeletePlayerById(){
+    this.playerController.deletePlayerById(100L);
+  }
+
 
 
 }
