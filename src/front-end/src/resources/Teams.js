425e670286ae60f31f073923e3b99e20e655181b
Kevin Xu <kevinx1@student.unimelb.edu.au>
2021-08-10 23:44:56 +1000

    Updated the team page

--- src/front-end/src/resources/Teams.js
+++ src/front-end/src/resources/Teams.js
@@ -7,16 +7,36 @@ import toolbarStyles from  './css/toolbar.module.css';
 import mcclogo from './img/mcc-logo.png';
 import { useHistory } from "react-router-dom";
 import Button from '@material-ui/core/Button';
-import MoreVertIcon from '@material-ui/icons/MoreVertOutlined';
+import profilepic from  './img/profile.png';
+import { TextField } from '@material-ui/core';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 function placeholderAlert() {
     return alert("Unsupported");
 }
 
 function Teams() {
-    const history = useHistory();
     const [response, setResponse] = useState({});
-    const [selectedTeam, setSelectedTeam] = useState(-1);
+    const [selectedTeam, setSelectedTeam] = useState({});
+    const [userSearchText, setUserSearchText] = useState("");
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [minPerformance, setMinPerformance] = useState(0);
+    const [maxPerformance, setMaxPerformance] = useState(10);
+    const [availability, setAvailability] = useState([]);
+    const [favPosition, setFavPosition] = useState([]);
+    const [sort, setSort] = useState("");
+    const [sortOrder, setSortOrder] = useState("");
+    const history = useHistory();
+    const [random, setRandom] = useState(Math.random());
+    const reRender = () => setRandom(Math.random());
+    const [updatingTeam, setUpdatingTeam] = useState(false);
 
     function membersHandleClick() {
         history.push("/members");
@@ -32,17 +52,13 @@ function Teams() {
 
     function renderTeams(data) {
         const teamsArray = data.data.data.teamList
-        console.log(teamsArray)
         return (
             <div className={bodyStyles.teamsList}>
                 {teamsArray.map(team => {
                     return (
-                        <div id={team.id} className={bodyStyles.teamCard} onClick={() => setSelectedTeam(team.id)}>
+                        <div style={team.id === selectedTeam.id ? {backgroundColor: 'grey', color: 'white'}: {}} className={bodyStyles.teamCard} onClick={() => setSelectedTeam(team)}>
                             <div className={bodyStyles.teamCardText}>
-                                Team number: {team.id}
-                            </div>
-                            <div className={bodyStyles.teamCardEdit}>
-                                <MoreVertIcon/>
+                                {team.teamName}
                             </div>
                         </div>
                     )
@@ -51,21 +67,273 @@ function Teams() {
         )
     }
 
-    function renderTeamComposition() {
+    function playerCard(name) {
         return (
-            <div>
-                {selectedTeam}
+            <>
+                <div className={bodyStyles.selectTeamColumnBoxCard}>
+                    <div className={bodyStyles.selectTeamColumnBoxCardImage}>
+                        <img style={{width: '100%', objectFit: 'contain'}} src={profilepic} alt="Logo" />
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBoxCardText}>
+                        {name}
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+    function renderTeamComposition(res) {
+        return (
+            <>
+                <div className={bodyStyles.selectTeamColumnContainer}>
+                    <div className={bodyStyles.selectTeamColumnHeader}>
+                        Skip
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.skipBowlerName1)}
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.skipBowlerName2)}
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.skipBowlerName3)}
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.skipBowlerName4)}
+                    </div>
+                </div>
+                <div className={bodyStyles.selectTeamColumnContainer}>
+                    <div className={bodyStyles.selectTeamColumnHeader}>
+                        Third
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.thirdBowlerName1)}
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.thirdBowlerName2)}
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.thirdBowlerName3)}
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.thirdBowlerName4)}
+                    </div>
+                </div>
+                <div className={bodyStyles.selectTeamColumnContainer}>
+                    <div className={bodyStyles.selectTeamColumnHeader}>
+                        Second
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.secondBowlerName1)}
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.secondBowlerName2)}
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.secondBowlerName3)}
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.secondBowlerName4)}
+                    </div>
+                </div>
+                <div className={bodyStyles.selectTeamColumnContainer}>
+                    <div className={bodyStyles.selectTeamColumnHeader}>
+                        Lead
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.leadBowlerName1)}
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.leadBowlerName2)}
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.leadBowlerName3)}
+                    </div>
+                    <div className={bodyStyles.selectTeamColumnBox}>
+                        {playerCard(res.leadBowlerName4)}
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+    function handleFilterClickOpen() {
+        setDialogOpen(true);
+    }
+
+    function handleFilterClickClose() {
+        setDialogOpen(false);
+    }
+
+    function renderSelectTeamComponent() {
+        return (
+            <div className={bodyStyles.selectTeam}>
+                <div className={bodyStyles.selectTeamMain}>
+                    {renderTeamComposition(selectedTeam)}
+                </div>
+                <div className={bodyStyles.selectTeamButton}>
+                    <Button variant="contained" color="primary" onClick={() => setUpdatingTeam(true)}>Update this team</Button>
+                </div>
             </div>
         )
     }
 
+    function renderSearchBarContainer() {
+        return (
+            <div className={toolbarStyles.searchBarContainer}>
+                <div className={toolbarStyles.searchBar}>
+                    <TextField style={{width: '100%'}} onChange={(e) => {setUserSearchText(e.target.value)}} variant="outlined" label="Search User"/>
+                </div>
+                <div className={toolbarStyles.filter}>
+                    <Button variant="contained" colour="primary" onClick={handleFilterClickOpen}>Filter</Button>
+                    <Dialog className={toolbarStyles.filterDialog} open={dialogOpen} onClose={handleFilterClickClose}>
+                        <DialogTitle>Filters Results</DialogTitle>
+                        <DialogContent className={toolbarStyles.filterDialogContent}>
+                            <FormControl className={toolbarStyles.filterFormControl} style={{marginRight: "10%"}}>
+                            <InputLabel shrink id="sort label">Sort</InputLabel>
+                                <Select
+                                    label="Sort"
+                                    id="sort"
+                                    value={sort}
+                                    displayEmpty
+                                    onChange={(e) => {setSort(e.target.value)}}
+                                >
+                                    <MenuItem value={'name'}>Name</MenuItem>
+                                    <MenuItem value={'recentPerformance'}>Recent Performance</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <FormControl className={toolbarStyles.filterFormControl}>
+                            <InputLabel shrink id="sort order label">Sort Order</InputLabel>
+                                <Select
+                                    label="Sort order"
+                                    id="sort-order"
+                                    value={sortOrder}
+                                    displayEmpty
+                                    onChange={(e) => {setSortOrder(e.target.value)}}
+                                >
+                                    <MenuItem value={'asc'}>Ascending</MenuItem>
+                                    <MenuItem value={'desc'}>Descending</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </DialogContent>
+                        <DialogContent className={toolbarStyles.filterDialogContent}>
+                            <FormControl className={toolbarStyles.filterFormControl} style={{marginRight: "10%"}}>
+                            <InputLabel shrink id="min performance label">Min Performance</InputLabel>
+                                <Select
+                                    label="Minimum"
+                                    id="performance-min"
+                                    value={minPerformance}
+                                    displayEmpty
+                                    onChange={(e) => {setMinPerformance(e.target.value)}}
+                                >
+                                    <MenuItem value={0}>0</MenuItem>
+                                    <MenuItem value={1}>1</MenuItem>
+                                    <MenuItem value={2}>2</MenuItem>
+                                    <MenuItem value={3}>3</MenuItem>
+                                    <MenuItem value={4}>4</MenuItem>
+                                    <MenuItem value={5}>5</MenuItem>
+                                    <MenuItem value={6}>6</MenuItem>
+                                    <MenuItem value={7}>7</MenuItem>
+                                    <MenuItem value={8}>8</MenuItem>
+                                    <MenuItem value={9}>9</MenuItem>
+                                    <MenuItem value={10}>10</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <FormControl className={toolbarStyles.filterFormControl} disabled={minPerformance===""} >
+                                <InputLabel shrink id="availability label">Max Performance</InputLabel>
+                                <Select
+                                    id="availability select"
+                                    value={maxPerformance}
+                                    onChange={(e) => {setMaxPerformance(e.target.value)}}
+                                    displayEmpty
+                                >
+                                    {(() => {
+                                        const options = [];
+                                        for (let i = minPerformance; i<=10; i++) {
+                                            options.push(<MenuItem value={i}>{i}</MenuItem>)
+                                        }
+                                        return options;
+                                    }
+                                    )()}
+                                </Select>
+                            </FormControl>
+                        </DialogContent>
+                        <DialogContent className={toolbarStyles.fullDialogContent}>
+                            <FormControl className={toolbarStyles.availabilityFormControl}>
+                            <InputLabel shrink id="availability label">Availabilities</InputLabel>
+                            <Select
+                                labelId="availability label"
+                                id="availability"
+                                multiple
+                                value={availability}
+                                onChange={(e) => {setAvailability(e.target.value)}}
+                            >
+                                {["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].map((day) => (
+                                    <MenuItem key={day} value={day}>{day}</MenuItem>
+                                ))}
+                            </Select>
+                            </FormControl>
+                        </DialogContent>
+                        <DialogContent className={toolbarStyles.fullDialogContent}>
+                            <FormControl className={toolbarStyles.favPositionFormControl}>
+                            <InputLabel shrink id="fav position label">Favourite Position</InputLabel>
+                            <Select
+                                labelId=" fav position label"
+                                id="favourite position"
+                                multiple
+                                value={favPosition}
+                                onChange={(e) => {setFavPosition(e.target.value)}}
+                            >
+                                {["Skip","Second","Third","Lead"].map((position) => (
+                                    <MenuItem key={position} value={position}>{position}</MenuItem>
+                                ))}
+                            </Select>
+                            </FormControl>
+                        </DialogContent>
+                        {/* <DialogContent className={toolbarStyles.fullDialogContent}>
+                            <FormControl className={toolbarStyles.preferenceFormControl}>
+                                <InputLabel shrink>Preferences</InputLabel>
+                                <Input id="preference" value={preference} onChange={(e) => {setPreference(e.target.value)}} />
+                            </FormControl>
+                        </DialogContent> */}
+                        <DialogActions>
+                            <Button onClick={handleFilterClickClose} color="primary">
+                                Go Back
+                            </Button>
+                            <Button onClick={() => {handleFilterClickClose(); reRender()}} color="primary">
+                                Submit
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+                </div>
+            </div>
+        )
+    }
+
+    function teamViewingMode() {
+        return (
+            <div className={bodyStyles.teamsBody}>
+                <div className={bodyStyles.teamsListContainer}>
+                    {response !== {} && response.status === 200 && renderTeams(response)}
+                    {response === {} && <div>...Loading</div>}
+                </div>
+                {Object.keys(selectedTeam).length !== 0 && selectedTeam.consructor !== Object && renderSelectTeamComponent()}
+            </div>
+        )
+    }
+
+    function teamEditingMode() {
+        return (
+            <div>Editing mode</div>
+        )
+    }
+
     useEffect(() => {
-        axios.get(`http://128.199.253.108:8082/team/gerAllTeam`)
+        axios.get(`http://128.199.253.108:8082/team/getAllTeam`)
             .then(res => {
                 setResponse(res);
-                console.log(res);
             })
-    }, []);
+    }, [random]);
 
     return (
         <div style={{height: '100vh', display: 'flex', flexFlow: 'column'}}>
@@ -85,18 +353,12 @@ function Teams() {
             </div>
             <div className={toolbarStyles.toolbar}>
                 <div className={toolbarStyles.newUserContainer}>
-                    <Button variant="contained" color="primary" onClick={placeholderAlert}>New Team</Button>
+                    <Button variant="contained" color="primary" onClick={placeholderAlert}>Create Team</Button>
                 </div>
+                {updatingTeam === true && renderSearchBarContainer()}
             </div>
-            <div className={bodyStyles.teamsBody}>
-                <div className={bodyStyles.teamsListContainer}>
-                    {response !== {} && response.status === 200 && renderTeams(response)}
-                    {response === {} && <div>...Loading</div>}
-                </div>
-                <div className={bodyStyles.selectTeam}>
-                    {selectedTeam !== -1 && renderTeamComposition()}
-                </div>
-            </div>
+            {updatingTeam === false && teamViewingMode()}
+            {updatingTeam === true && teamEditingMode()}
         </div>
     )
 }
