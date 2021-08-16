7f8f62a16acbad60580aff2d0c1d187e0e957492
Kevin Xu <kevinx1@student.unimelb.edu.au>
2021-08-16 23:58:51 +1000

    Revert "Competition-V1"
    This reverts commit bc55c7c88665665fcb9004c4223c1e261c2eae4e.

--- src/front-end/src/resources/Home.js
+++ src/front-end/src/resources/Home.js
@@ -1,115 +1,45 @@
-import React,{Component} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-
-import Button from '@material-ui/core/Button';
+import React from 'react';
 import styles from './css/navbar.module.css';
 import mcclogo from './img/mcc-logo.png';
-import './css/home.css';
-import { Switch,Route,Redirect } from 'react-router-dom';
-import MidweekPennant from './other/WeekendPennat';
-import WeekendPennat from './other/MidweekPennant';
+import { useHistory } from "react-router-dom";
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      width: '100%',
-      maxWidth: 360,
-      backgroundColor: theme.palette.background.paper,
-    },
-    nested: {
-      paddingLeft: theme.spacing(4),
-    },
-  }));
-  
-
-class Home extends Component{
-    constructor(props){
-        super(props)
-    }
-
-    state = {
-        leftNav: [
-            {name: 'WeekEnd Pennant',url: '/home/midweekPennant'},
-            {name: 'Midweek Pennant',url: '/home/weekendPennat'},
-            {name: 'Thursday 7-A-Side',url: ''},
-            {name: 'Night Pennant',url: ''},
-        ],
-        activeNavIndex: 0
-    }
-    membersHandleClick = () => this.props.history.push("/members");
-    homeHandleClick = () => this.props.history.push("/home");
-    teamsHandleClick = () => this.props.history.push("/teams");
-    placeholderAlert = () => alert("Unsupported");
-
-    headerView = () => {
-        return (
-            <div className={styles.body}>
-                <div className={styles.logotext} >
-                    <img className={styles.mcclogo} onClick={this.homeHandleClick} src={mcclogo} alt="Logo" />
-                </div>
-                <div className={styles.linktabs}>
-                    <Button className={styles.linkbuttons} onClick={this.placeholderAlert}>COMPETITION</Button>
-                    <Button className={styles.linkbuttons} onClick={this.teamsHandleClick}>TEAMS</Button>
-                    <Button className={styles.linkbuttons} onClick={this.membersHandleClick}>MEMBERS</Button>
-                    <Button className={styles.linkbuttons} onClick={this.placeholderAlert}>SELECTION COMMITTEE</Button>
-                </div>
-                <div className={styles.logout}>
-                    <Button onClick={this.placeholderAlert}>LOG OUT</Button>
-                </div>
-            </div>
-        )
-    }
-    pageSwitch = (url,index) => {
-        this.setState({activeNavIndex:index})
-        console.log(url,this.props);
-        if(url){
-            this.props.history.push(url)
-        }
-    }
-    silderItem = ({name,url,index}) => {
-        return (
-            <ListItem selected={this.state.activeNavIndex == index} button key={index} onClick={() => this.pageSwitch(url,index)}>
-                <ListItemText style={{fontSize: '28px',color: 'teal',fontWeight: 'bold'}} primary={name} />
-            </ListItem>
-        )
-    }
-
-    componentDidMount(){
-        const {pathname} = this.props.history.location; 
-        const activeNavIndex = this.state.leftNav.findIndex(item => item['url'] == pathname);
-        if(activeNavIndex != -1){
-            this.setState({activeNavIndex})
-        }
-    }
-
-    render(){
-        const {leftNav} =this.state;
-        return (
-            <div className="home">
-                <header>{this.headerView()}</header>
-                <main className="main">
-                    <section className="silder">
-                        <List component="nav">
-                            {leftNav.map((item,index) => this.silderItem({...item,index}))}
-                        </List>
-                    </section>
-                    <section className="body">
-                        <Switch>
-                            <Route exact path="/home/midweekPennant" component={()=><MidweekPennant/>} />
-                            <Route exact path="/home/weekendPennat" component={()=> <WeekendPennat/>}/>
-                            <Route exact path="/home">
-                                <Redirect to="/home/weekendPennat" />
-                            </Route>
-                        </Switch>
-                    </section>
-                </main>
-            </div>
-        );
-    }
+function placeholderAlert() {
+    return alert("Unsupported");
 }
+
+function Home() {
+
+    const history = useHistory();
+
+    function membersHandleClick() {
+        history.push("/members");
+    };
+
+    function homeHandleClick() {
+        history.push("/home");
+    };
+
+    function teamsHandleClick() {
+        history.push("/teams")
+    };
+
+    return (
+        <div className={styles.body}>
+            <div className={styles.logotext} >
+                <img className={styles.mcclogo} onClick={homeHandleClick} src={mcclogo} alt="Logo" />
+            </div>
+            <div className={styles.linktabs}>
+                <Button className={styles.linkbuttons} onClick={placeholderAlert}>COMPETITION</Button>
+                <Button className={styles.linkbuttons} onClick={teamsHandleClick}>TEAMS</Button>
+                <Button className={styles.linkbuttons} onClick={membersHandleClick}>MEMBERS</Button>
+                <Button className={styles.linkbuttons} onClick={placeholderAlert}>SELECTION COMMITTEE</Button>
+            </div>
+            <div className={styles.logout}>
+                <Button onClick={placeholderAlert}>LOG OUT</Button>
+            </div>
+        </div>
+    );
+};
 
 export default Home;
--- src/front-end/src/routes.js
+++ src/front-end/src/routes.js
@@ -4,19 +4,17 @@ import Home from './resources/Home';
 import Profile from './resources/Profile';
 import Teams from './resources/Teams';
 import CreateProfile from './resources/CreateProfile';
-import MoreTeams from './resources/other/MoreTeams';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 export const Routes = () => {
   return (
     <div>
       <Switch>
-        <Route path="/home" component={Home} />
+        <Route exact path="/home" component={Home} />
         <Route exact path="/teams" component={Teams}/>
         <Route exact path="/members" component={Members} />
         <Route exact path="/members/create" component={CreateProfile} />
         <Route exact path="/members/:id" component={Profile} />
-        <Route exact path="/moreTeams" component={MoreTeams} />
         <Route exact path="/">
           <Redirect to="/home" />
         </Route>
