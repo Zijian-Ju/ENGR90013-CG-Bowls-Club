78de10b3158ac42b35c2fdf3a096d3997f0a7714
Kevin Xu <kevinx1@student.unimelb.edu.au>
2021-09-30 22:51:57 +1000

    Completed committee.js refactor for api

--- src/front-end/src/resources/API.js
+++ src/front-end/src/resources/API.js
@@ -17,5 +17,20 @@ export const API = {
     async getPermissions(token, email) {
         const response = await axios.get(`http://128.199.253.108:8082/sso/getUserPermession`, {headers: {"Access-Token": token, "Email": email}})
         return response;
+    },
+
+    async createSelector(email, id, password, realName, role, token, tokenCreateDate, userName, accessToken, accessEmail) {
+        const response = await axios.post(`http://128.199.253.108:8082/sso/addUser`, {email: email, id: id, password: password, realName: realName, role: role, token: token, tokenCreateDate: tokenCreateDate, userName: userName}, {headers: {"Access-Token": accessToken, "Email": accessEmail}})
+        return response;
+    },
+
+    async deleteSelector(user, accessToken, accessEmail) {
+        const response = await axios.post(`http://128.199.253.108:8082/sso/deleteUser`, user, {headers: {"Access-Token": accessToken, "Email": accessEmail}})
+        return response;
+    },
+
+    async getAllUser(accessToken, accessEmail) {
+        const response = await axios.get(`http://128.199.253.108:8082/sso/getAllUser`, {headers: {"Access-Token": accessToken, "Email": accessEmail}})
+        return response;
     }
 }
