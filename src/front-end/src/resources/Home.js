4972bbd0ab77ca9e35954f8113bcbac06653aff3
Kevin Xu <kevinx1@student.unimelb.edu.au>
2021-10-07 22:24:44 +1100

    Added home page

--- src/front-end/src/resources/Home.js
+++ src/front-end/src/resources/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import NavBar from './NavBar';
+import homeStyles from './css/home.module.css';
 
 function Home() {
 
     return (
-        <NavBar/>
+        <div style={{height: '100vh', display: 'flex', flexFlow: 'column'}}>
+            <NavBar/>
+            <div className={homeStyles.body}>
+                <div style={{height: '70%'}}>
+                </div>
+                <div className={homeStyles.welcome}>
+                    Welcome to MCC
+                </div>
+            </div>
+        </div>
     );
 };
 
